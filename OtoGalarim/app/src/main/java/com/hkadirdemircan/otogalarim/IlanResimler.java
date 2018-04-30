5396f7199ac71fe3c8ac61bea7397a3f380e231a
HKDemircan <hkdemircan06@gmail.com>
2018-04-30 21:31:29 +0300

    araç ilan yayınlamak için layout tasarımları tamamlandı.

---
+++ OtoGalarim/app/src/main/java/com/hkadirdemircan/otogalarim/IlanResimler.java
@@ -0,0 +1,13 @@
+package com.hkadirdemircan.otogalarim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IlanResimler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ilan_resimler);
+    }
+}
---
+++ OtoGalarim/app/src/main/java/com/hkadirdemircan/otogalarim/Yakit.java
@@ -0,0 +1,13 @@
+package com.hkadirdemircan.otogalarim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Yakit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yakit);
+    }
+}
